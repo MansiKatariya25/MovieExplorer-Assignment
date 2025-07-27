846816123241a4a50384e7df634a4c4ca34bfc22
katariyamansi25 <katariyamansi25@gmail.com>
2025-07-28 02:10:34 +0530

    added next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,16 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        port: '',
-        pathname: '/t/p/**',
-      },
+      { protocol: 'https', hostname: 'image.tmdb.org', pathname: '/t/p/**' },
     ],
     unoptimized: true,
   },
-  experimental: {
-    optimizePackageImports: ['lucide-react'],
-  },
-  devIndicators: false
-}
+  experimental: { optimizePackageImports: ['lucide-react'] },
+  devIndicators: false,
+};
 
-export default nextConfig
+export default nextConfig;
